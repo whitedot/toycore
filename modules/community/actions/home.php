8b56f7c83ce141bf3b60b6f033c0a8d1532b86dc
whitedot <whitedot@naver.com>
2026-05-08 23:24:32 +0900

    fix: 커뮤니티 홈 게시판 권한 노출 수정

--- modules/community/actions/home.php
+++ modules/community/actions/home.php
@@ -6,7 +6,12 @@ require_once TOY_ROOT . '/modules/member/helpers.php';
 require_once TOY_ROOT . '/modules/community/helpers.php';
 
 $account = toy_member_current_account($pdo);
-$boards = toy_community_enabled_boards($pdo);
+$boards = [];
+foreach (toy_community_enabled_boards($pdo) as $board) {
+    if (toy_community_account_can_read_board($pdo, $board, is_array($account) ? $account : null)) {
+        $boards[] = $board;
+    }
+}
 $settings = toy_module_settings($pdo, 'community');
 $themeKey = toy_community_theme_key($settings);
 $themeView = toy_community_theme_view($themeKey, 'home');
